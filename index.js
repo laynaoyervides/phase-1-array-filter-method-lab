c0007102e13ca6aab667e824098f086b420f6b9f
Layna Oyervides <laynaoyervides@gmail.com>
2021-08-21 13:33:14 -0600

    commit tree 540e0a44996a6962365db1e1db26038c2d18f1ff

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solution here
+const drivers = [
+    {
+      name: 'Bobby',
+      hometown: 'Pittsburgh' },
+    {
+      name: 'Sammy',
+      hometown: 'New York' } ,
+    {
+      name: 'Sally',
+      hometown: 'Cleveland' },
+    {
+      name: 'Annette',
+      hometown: 'Los Angeles' },
+    {
+      name: 'Bobby',
+      hometown: 'Tampa Bay' }
+  ];
+
+function findMatching(drivers, name){
+    const exactName = function(person){
+        return person.toLowerCase() === name.toLowerCase();
+    }
+        return drivers.filter(exactName);
+    }
+
+
+function fuzzyMatch(drivers, name){
+    function test(first2){
+        let name2 = name.substring(0.2);
+        return first2.substring(0,2) === name2;
+    }
+    return drivers.filter(test);
+
+}
+
+function matchName(drivers, string){
+    const driversWho = function(drivers){
+        return drivers.name === string;
+    }
+    return drivers.filter(driversWho);
+}
